edccbca93b2a38efeca43aa45f3184996558f97a
a2kaido <amane.nikaido+a2kaido@gmail.com>
2020-06-22 22:56:57 +0900

    null

--- app/src/main/java/io/github/a2kaido/nullobject/MainActivity.kt
+++ app/src/main/java/io/github/a2kaido/nullobject/MainActivity.kt
@@ -2,15 +2,11 @@ package io.github.a2kaido.nullobject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        main_text.setOnClickListener {
-            startActivity(SubActivity.buildIntent(this, NullParent.Null))
-        }
+        println(Null.`null`)
     }
 }
--- app/src/main/java/io/github/a2kaido/nullobject/NullParent.kt
+++ app/src/main/java/io/github/a2kaido/nullobject/NullParent.kt
@@ -1,13 +1,7 @@
 package io.github.a2kaido.nullobject
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-sealed class NullParent : Parcelable {
-    @Parcelize
-    object Null : NullParent()
-
-    companion object {
-        val list = listOf(Null)
-    }
+abstract class NullParent {
+    val `null` = Null
 }
+
+object Null : NullParent()
